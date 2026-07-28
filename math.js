5974d2b9fad6b852c970fc23449af5eba7dc0349
Abhishek Nigam <nabhishek1947@ibm.com>
2026-07-29 00:08:20 +0530

    first commit

---
+++ math.js
@@ -0,0 +1,15 @@
+function add(a, b){
+    return a + b;
+}
+
+function mul(a, b){
+    return a * b;
+}
+
+
+module.exports = {add, mul}
+
+// --------------------------------
+
+// exports.add = (a,b) => (a+b);   // anonymous function
+// exports.mul = (a,b) => (a*b);
